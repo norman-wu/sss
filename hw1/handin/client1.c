60b146591a5e18122777bc8569f1ef49115957f7
John Filleau <jfilleau@cmu.edu>
2014-02-09 14:59:13 -0500

    Added handin directory and tar

---
+++ hw1/handin/client1.c
@@ -0,0 +1,113 @@
+/* file: client1.c class: 18-732, Spring 2013, assignment: Homework 1
+*/
+
+/* Obligatory includes */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <string.h>
+#include <sys/time.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/uio.h>
+#include <stdarg.h>
+#include <errno.h>
+
+
+int main(int argc, char** argv)
+{
+  int debug = 0;
+  char reqstring[1000] = "IMG:abcd.jpg;LAT:57.64911;LON:10.40744;CAP:Good morning!";
+  ;
+  int PORTNUM;
+  char SERVER_IP[16];
+
+  int sock, nbytes, i, total, s;
+  char request[1000];
+  char recvline[1000];
+  struct sockaddr_in srv;
+
+  /* Set up some defaults for if you don't enter any parameters */
+  PORTNUM = 18732;
+  strcpy(SERVER_IP, "127.0.0.1");
+
+  printf("\nUsage: client [-port <port_number>] [-server <server_IP>]\n");
+
+  /* Process command line switches */
+  /* Usage: client [-port <port_number>] [-server <server_IP>] */
+  for(i = 1; i < argc; i++){
+    if(argv[i][0] == '-'){
+      if(strcmp(argv[i], "-port") == 0){
+		    PORTNUM = atoi(argv[++i]);
+      }else if(strcmp(argv[i], "-server") == 0){
+	strncpy(SERVER_IP, argv[++i],16);
+      }else if(strcmp(argv[i], "-d") == 0){
+	debug = 1;
+      }else{
+	printf("Unknown switch \"%s\"\n", argv[i]);
+	exit(1);
+      }
+    }else{
+      if (debug) {
+	strncpy(reqstring, argv[i], 1000);
+	printf("%s\n", argv[i]);
+      } else {
+	printf("Unknown switch \"%s\"\n", argv[i]);
+	exit(1);
+      }
+    }
+  }
+
+  /* Fill in the server data structure */
+  memset(&srv, 0, sizeof(srv));
+  srv.sin_family = AF_INET;
+  srv.sin_port = htons(PORTNUM);
+  srv.sin_addr.s_addr = inet_addr(SERVER_IP);
+
+	/* Create the socket */
+  if((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0){
+    perror("socket");
+		exit(1);
+  }
+
+  printf("\nConnecting to %s:%u\n", SERVER_IP, PORTNUM);
+
+  /* Connect to the socket */
+  if(connect(sock, (struct sockaddr*) &srv, sizeof(srv)) < 0){
+    perror("connect");
+    exit(1);
+  }
+
+  printf("The request is:\n%s\n", reqstring);
+
+  /* Make the request */
+  strncpy(request, reqstring,1000 );
+  total = 0;
+  s = strlen(request);
+  while( total < s){
+    nbytes = write(sock, request + total, s);
+    total = total + nbytes;
+  }
+
+  printf("The response of the server is:\n");
+
+  /* Get and output the response */
+  nbytes = 0;
+  while( (nbytes = read(sock, recvline, 1000)) > 0){
+    recvline[nbytes] = 0;
+    printf("%s\n", recvline);
+  }
+
+
+
+  return(0);
+}
+
