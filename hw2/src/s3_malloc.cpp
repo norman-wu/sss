0857dfae3ed309e6dbf79fb42ceb92fd4ab04373
Deanna Zhu <Deanna.Zhu514@gmail.com>
2014-03-23 02:35:18 -0400

    fixed spacing for s3

--- hw2/src/s3_malloc.cpp
+++ hw2/src/s3_malloc.cpp
@@ -21,40 +21,32 @@ ANALYZE_TREE()
     Pointer p;
 
     if(MATCH( lv_sp= malloc(_))) {
-	SET_STATE(lv_sp, 1);
-
-        ADD_EVENT(lv_sp, "malloc", "malloc assigned to" << lv_sp);
-        cout << lv_sp << " malloc at " <<  current_file_lineno() << endl;
-	return;
+      SET_STATE(lv_sp, 1);
+      ADD_EVENT(lv_sp, "malloc", "malloc assigned to" << lv_sp);
+      cout << lv_sp << " malloc at " <<  current_file_lineno() << endl;
+      return;
     }  
-    else if (MATCH(IfPat(cond))){
-	int i;
-        if (GET_STATE(cond, i)) {
-          if (MATCH_STATE(cond, 1)) {
-	    SET_STATE(cond, 0);
-	    ADD_EVENT(cond, "check", "malloc checked" << cond);
-	  }
-
-
-        } 
-        return;
+    else if (MATCH(IfPat(cond))) {
+      int i;
+      if (GET_STATE(cond, i)) {
+        if (MATCH_STATE(cond, 1)) {
+	       SET_STATE(cond, 0);
+	       ADD_EVENT(cond, "check", "malloc checked" << cond);
+	       }
+      } 
+      return;
     }
-    else if (MATCH(Contains(p) )) {
-	int i;
-
-	if (GET_STATE(p, i)) {
-
-          if (MATCH_STATE(p, 1)) {
-            SET_STATE(p, 2);     
-	    COMMIT_ERROR( p, "malloc", "malloc was not checked for return value at " << current_file_lineno() );
-	  }
-
-	
-       } 
-	return;
+    else if (MATCH(Contains(p))) {
+      int i;
+    	if (GET_STATE(p, i)) {
+        if (MATCH_STATE(p, 1)) {
+          SET_STATE(p, 2);     
+          COMMIT_ERROR( p, "malloc", "malloc was not checked for return value at " << current_file_lineno() );
+	       }
+      } 
+      return;
     }
   }                                                      
-
 }
 
 END_EXTEND_CHECKER();
